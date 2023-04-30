8ec92f98a27459e83708ae256dddc351a9435e7a
Esther White <monacodelisa@gmail.com>
2023-04-30 12:32:41 +0300

    mat-expansion-indicator

--- payarc-angular/src/app/components/payarc-training/payarc-training.component.ts
+++ payarc-angular/src/app/components/payarc-training/payarc-training.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./payarc-training.component.scss']
 })
 export class PayarcTrainingComponent {
+  isPanelOpen: { [index: number]: boolean } = {};
 
+  onPanelOpen(panelIndex: number): void {
+    this.isPanelOpen[panelIndex] = true;
+  }
+
+  onPanelClose(panelIndex: number): void {
+    this.isPanelOpen[panelIndex] = false;
+  }
 }
